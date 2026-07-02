6fe0b5768d8b1a0d257a2eaeebf0e37d626b6c4d
Geetha Ranganathan <41512774+Geetha12R@users.noreply.github.com>
2026-07-02 17:47:19 +0530

    collapse functionality added

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -7,7 +7,7 @@ import Grid  from "@mui/material/Grid";
 
 function Section({title,api}) {
   const [albums, setAlbums] = useState([]);
-  const [showAll, setShowAll] = useState(false);
+  const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
     axios
